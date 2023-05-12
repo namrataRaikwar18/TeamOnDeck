971bf27ed99c495d284d12b27ac1edaa5b0de3ee
Shrutigupta431 <sg9956855@gmail.com>
2023-05-12 16:59:35 +0530

    refactor: file change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      App Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit id distinctio sunt facilis nesciunt? Ullam reprehenderit sint odit, harum molestiae at maiores atque quam? Accusantium exercitationem nemo, amet impedit libero, nesciunt quibusdam obcaecati, iste magni temporibus similique? Vero officia commodi suscipit assumenda optio provident ipsam architecto ad delectus atque, et quasi alias mollitia nulla fuga.
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
