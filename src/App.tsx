ce540a7d511ffe520b5f95486d31709feb926367
Shrutigupta431 <sg9956855@gmail.com>
2023-05-12 16:58:09 +0530

    chore:created folders

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      App
+      App Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit id distinctio sunt facilis nesciunt? Ullam reprehenderit sint odit, harum molestiae at maiores atque quam? Accusantium exercitationem nemo, amet impedit libero, nesciunt quibusdam obcaecati, iste magni temporibus similique? Vero officia commodi suscipit assumenda optio provident ipsam architecto ad delectus atque, et quasi alias mollitia nulla fuga.
     </div>
   );
 }
